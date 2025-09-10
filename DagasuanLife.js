ccaa3254bf7f1536c5a9dbac25b355ee59a0e590
jhonlorddagasuan <jhonlorddagasuan@gmail.com>
2025-09-10 10:09:51 +1000

    Added student variables

---
+++ DagasuanLife.js
@@ -0,0 +1,10 @@
+// Step 1: Variables
+// Declare variables for your name, course, and year level.
+const studentName = "Jhonlord Dagasuan";
+const course = "Bachelor of Science in Information Technology";
+const yearLevel = "2nd Year";
+
+// Print them in the console.
+console.log("Name:", studentName);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
