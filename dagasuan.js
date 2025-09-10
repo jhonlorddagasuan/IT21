e0d1f63e9c3961c25d4839f05d10fd884a6b04de
jhonlorddagasuan <jhonlorddagasuan@gmail.com>
2025-09-10 10:26:48 +1000

    added step 1

---
+++ dagasuan.js
@@ -0,0 +1,7 @@
+let name = "Zyeus Medado";
+let course = "Bachelor Of Science in Technology";
+let yearLevel = "2nd Year";
+
+console.log("Name: " + name);
+console.log("Course: " + course);
+console.log("Year Level: " + yearLevel); // fixed here
